a1936bce030dd9ad68906734f693e66285c2f351
Alvaro <alvaroespi2003@gmail.com>
2024-12-17 08:55:30 +0100

    17/12

--- Python/GitHubAutomation/githubAutoLib.py
+++ Python/GitHubAutomation/githubAutoLib.py
@@ -10,14 +10,29 @@ def clear():
 def delete_line():
     print("\033[1A\x1b[2K", end="")
 
-entries = []
+def connection_available():
+    try:
+        repo = git.Repo(sys.argv[1])
+        repo.git.execute("git status -uno")
+        print("Connection available. Starting script.")
+        return True
+    except OSError:
+        print("Connection not availabel. Try again.")
+        pass
+    except git.exc.InvalidGitRepositoryError:
+        print("Repository checked is invalid.")
+        pass
+    return False
+
+
+numbers = []
 names = []
 states = []
 paths = []
 
 
 def reload_repos():
-    entries.clear()
+    numbers.clear()
     names.clear()
     states.clear()
     paths.clear()
@@ -38,7 +53,8 @@ def reload_repos():
                             state = "Up to date"
                     else:
                         state = "Changed"
-                    entries.append(i)
+                    repo.close()
+                    numbers.append(i)
                     names.append(repo_name)
                     states.append(state)
                     paths.append(arg)
@@ -50,13 +66,14 @@ def reload_repos():
                 print(f" {i}.) Path {arg} not found")
 
 
-def print_menu():
+
+def open_main_menu():
     states1 = []
     for st in states:
         states1.append("["+st+"]")
     max_st = max(states1, key=len)
     max_nm = max(names, key=len)
-    for (a, b, c, d) in zip(entries, states1, names, paths):
+    for (a, b, c, d) in zip(numbers, states1, names, paths):
         while len(b) < len(max_st):
             b += str(" ")
         while len(c) < len(max_nm):
@@ -66,7 +83,8 @@ def print_menu():
     print(" 0.) Exit.")
 
 
-def access_repo(opt:str):
+
+def open_repository_menu(opt:str):
     for i, arg in enumerate(sys.argv):
         if i==int(opt):
             try:
@@ -85,35 +103,30 @@ def access_repo(opt:str):
                     print("\t\t 0. Return.")
                     option = input("\tChoose an option: ")
                     if option=='0':
-                        print("Exiting...")
-                        input("Press Enter to continue...")
+                        input("Exiting. Press Enter to continue...")
                     elif option in str(opt_nums):
-                        if option == '1':
-                            git_command(arg, "Print", "git status")
-                        elif option == '2':
-                            git_command(arg, "Changes have been retreived from origin.", "git pull")
-                        elif option == '3':
-                            git_command(arg, "Recent changes have been discarded.", "git stash")
+                        if option == '1':   git_command(arg, "Print", "git status")
+                        elif option == '2': git_command(arg, "Changes have been retreived from origin.", "git pull")
+                        elif option == '3': git_command(arg, "Recent changes have been discarded.", "git stash")
                         elif option == '4':
                             commit_name = input("\nWhat is the name of the commit?: ")
-                            commit = "git commit -m \""+commit_name+"\""
-                            git_command(arg, "Changes have been saved.", "git add --all", commit)
+                            git_command(arg, "Changes have been saved.", "git add --all", "git commit -m \""+commit_name+"\"")
                         elif option == '5':
                             commit_name = input("\nWhat is the name of the commit?: ")
-                            commit = "git commit -m \""+commit_name+"\""
-                            git_command(arg, "Changes have been saved and pushed to origin.", "git add --all", commit, "git push")
+                            git_command(arg, "Changes have been saved and pushed to origin.", "git add --all", "git commit -m \""+commit_name+"\"", "git push")
                         reload_repos()
                         clear()
                         option = "0"
                     else:
-                        print("Not valid input.")
-                        input("Press Enter to continue...")
+                        input("Not valid input. Press Enter to return...")
                         i=1
                         while i<len(opt_nums)+5:
                             delete_line()
                             i += 1
+                repo.close()
             except git.exc.InvalidGitRepositoryError:
                 print(f"Repository {arg} not found")
+
 
 
 def git_command(arg, message, *commands):
@@ -127,30 +140,32 @@ def git_command(arg, message, *commands):
             repo.git.execute(comm)
         print(message)
     input("Press Enter to continue...")
+    repo.close()
 
 
 
 
 if __name__ == "__main__":
 
-    opt = "1"
-    while opt!="0":
+    if connection_available():
 
-        clear()
-        if (entries == []):
-            reload_repos()
+        opt = "1"
+        while opt!="0":
+            clear()
+            if (numbers == []):
+                reload_repos()
             
-        print("\n--- Repository List ---\n")
-        print_menu()
-        opt = input("\nChoose a repo: ")
+            print("\n--- Repository List ---\n")
+            open_main_menu()
+            opt = input("\nChoose a repo: ")
 
-        if opt=="U" or opt=="u":
-            reload_repos()
-        elif opt=="0":
-            print("\nExiting...")
-        elif opt in str(entries):
-            access_repo(opt)
-        else:
-            print("Option not valid")
-            input("Press Enter to continue...")
+            if opt=="U" or opt=="u":
+                reload_repos()
+            elif opt=="0":
+                print("\nExiting...")
+            elif int(opt) in numbers:
+                open_repository_menu(opt)
+            else:
+                print("Option not valid. Press Enter to return...")
+
 
